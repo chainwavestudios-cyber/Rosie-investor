438ccb89085353ace8590f4c6dcf652422ced1d7
Christopher <chainwavestudios@gmail.com>
2026-04-01 14:14:54 +0000

    File changes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen">
+      <iframe
+        src="https://rawcdn.githack.com/chainwavestudios-cyber/agentbmaninvest/main/agentbman-pitchbook-v3.html"
+        className="w-full h-full border-0"
+        title="AgentBman Pitchbook"
+      />
+    </div>
+  );
+}
